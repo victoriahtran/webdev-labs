9110de766e7d529bbf7c8c760ac64a850e34339d
Victoria Tran <victoriahcntran@gmail.com>
2022-06-06 14:43:33 -0500

    navbar

--- lab10/src/Navbar.js
+++ lab10/src/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { getHeaders } from './utils';
 
 class Navbar extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = { profile : [] }
+        this.fetchProfile = this.fetchProfile.bind(this)
         //initialization code here
         console.log("Navbar constructed")
     }
@@ -13,10 +16,26 @@ class Navbar extends React.Component {
         console.log("Navbar component mounted")
     }
 
+    fetchProfile() {
+        fetch('/api/profile', {
+            method: "GET",
+            headers: getHeaders()
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ profile: data })
+            })
+    }
+
     render () {
         return (
             <nav className="main-nav">
                 <h1>Photo App</h1>
+                <ul>   
+                    <li><a href="/api">API Docs</a></li>
+                    <li><span>{this.state.profile.username}</span></li>
+                    <li><a href="/logout">Sign out</a></li>
+                </ul>
             </nav>
         );
     }
